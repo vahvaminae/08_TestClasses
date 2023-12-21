835d592f206fdb006ebd9f3966894e2bd25db11c
vahvaminae <126572549+vahvaminae@users.noreply.github.com>
2023-12-21 08:12:36 +0000

    Rechner Klasse anlegen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,6 +3,9 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+
+
+    System.out.println("Hi");
      
     }
 
---
+++ com/cc/java/Rechner.java
@@ -0,0 +1,12 @@
+package com.cc.java;
+
+public class Rechner {
+
+ public int add(int a, int b)
+ 
+ {
+
+    return a + b;
+ }
+
+}
